de72ad113d144da45022e456a17d41b0f7cadb15
HenriqueGMen <henrimeen24@gmail.com>
2022-08-20 00:24:22 -0300

    create users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
 import { specificatonsRoutes } from './specifications.routes';
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificatonsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementation/CategoriesRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementation/SpecificationRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementation/SpecificationRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -14,3 +16,8 @@ container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
 );
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)
