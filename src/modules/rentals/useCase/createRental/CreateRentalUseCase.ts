ecfaf1c20a31b2fb0d4a8179706d9167eb32162b
HenriqueGMen <henrimeen24@gmail.com>
2022-09-07 01:07:48 -0300

    attRentals

--- src/modules/rentals/useCase/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCase/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -12,7 +13,7 @@ class CreateRentalUseCase{
     private rentalsRepository: IRentalsRepository
   ) {}
 
-  async execute({ user_id, car_id, expected_return_date}: IRequest): Promise<void> {
+  async execute({ user_id, car_id, expected_return_date}: IRequest): Promise<Rental> {
 
     const carUnavailable = await this. rentalsRepository.findOpenRentalByCar(car_id);
 
@@ -25,6 +26,14 @@ class CreateRentalUseCase{
     if(rentalOpenToUser) {
       throw new AppError("There's a rental in progress for this user!");
     }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   };
 }
 
