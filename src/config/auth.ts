426925fc24da6e1282fd649fcdf3aa886fa66734
HenriqueGMen <henrimeen24@gmail.com>
2022-09-24 18:51:03 -0300

    att and refactor authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "e6937f449d4dd7518fbf830e38cf8eb8",
+  expires_in_token: "15m", 
+  secret_refresh_token: "f945066221d056c6c84cb2558cd843d0",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+}
