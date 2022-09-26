97f339c12963be6b1d6695b98e8964a542ea010f
HenriqueGMen <henrimeen24@gmail.com>
2022-09-25 23:40:50 -0300

    att refreshToken

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+class RefreshTokennController {
+
+  async handle(req: Request, res: Response): Promise<Response> {} 
+}
+
+export { RefreshTokennController }
