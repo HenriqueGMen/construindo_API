ec5ceb52e8d56e2676cf16ace8621246747a11d9
HenriqueGMen <henrimeen24@gmail.com>
2022-09-27 18:14:01 -0300

    reset password

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -11,7 +11,7 @@ class  UsersTokensRepository implements IUsersTokensRepository {
   constructor() {
     this.repository = getRepository(UserTokens);
   }
-  
+    
   async create({ user_id, refresh_token, expires_date }: ICreateUserTokenDTO): Promise<UserTokens> {
     const userToken = this.repository.create({
       user_id,
@@ -35,6 +35,12 @@ class  UsersTokensRepository implements IUsersTokensRepository {
 
   async deleteById(id: string): Promise<void> {
     await this.repository.delete(id);
+  }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = await this.repository.findOne({ refresh_token });
+
+    return userToken;
   }
 }
 
